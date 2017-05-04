9bfb3881ba1cb3733f40b6038bbdfa5686e3911e
Tumble <tumblegamer@gmail.com>
2017-05-04 16:09:07 +0100

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,10 +1,18 @@
 @echo off
-ipconfig>ip.txt
-notepad ip.txt
+start notepad server.properties
+:restart
+cls
 del ip.txt
-rem java -Xmx1024M -Xms1024M -jar "S:\COMPUTER SCIENCE MINECRAFT SERVER\minecraft_server.jar" nogui
+set ip_address_string="IP Address"
+rem Uncomment the following line when using Windows 7 (with removing "rem")!
+set ip_address_string="IPv4 Address"
+echo Network Connection Test
+for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do echo Your IP Address is: %%f:25565&&echo %%f:25565>ip.txt
+
+rem start notepad ip.txt
 java -Xmx1024M -Xms1024M -jar "minecraft_server.jar"
 echo.
 echo.
 echo.
-pause
+echo Restarting...
+goto restart
